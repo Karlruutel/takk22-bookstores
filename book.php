3353178c312c0111c70a008b2d5f332e68002010
karlruutel <karl.ruutel>
2023-10-20 12:26:35 +0300

    2

--- book.php
+++ book.php
@@ -1,16 +1,6 @@
 <?php
 
-require_once("config.php");
-
-$id = $_GET["id"];
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-$pdo = new PDO($dsn, $user, $pass, $options);
+require_once("connect.php");
 
 $id = $_GET["id"];
 
@@ -21,3 +11,19 @@ $stmt->execute(["id" => $id]);
 $book = $stmt->fetch();
 
 var_dump($book);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?$book["title"]?> </h1>    
+    <span style="font-size 16px;">Aasta</span> <span style="font-size 24px;"><?=$book["release_date"] ?></span>
+
+</body>
+</html>
