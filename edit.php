e2fdf07225c208abff2f203329afc92b03a7679b
karlruutel <karl.ruutel@ametikool.ee>
2023-10-23 09:46:50 +0300

    8

--- edit.php
+++ edit.php
@@ -1,6 +1,19 @@
 <?php
 
+require_once("connect.php");
+
 $id = $_GET["id"];
+
+if ( isset($_POST["submit"]) && $_POST["submit"] == "save" )  {
+    $stmt = $pdo->prepare('UPDATE books SET tirle = :title WHERE id = :id');
+    $stmt->execute(["title" => $_POST["title"], "id" => $id]);
+    
+    header("Location: book.php?id=($id)");
+}
+
+$stmt = $pdo->prepare('SELECT * FROM books WHERE id = :id');
+$stmt->execute(["id" => $id]);
+$book = $stmt->fetch();
 
 ?>
 
@@ -12,8 +25,9 @@ $id = $_GET["id"];
     <title>Muuda</title>
 </head>
 <body>
-    <form action="edit,php" method="post">
-        <input type="text" name="title" value="">
+    <form action="edit.php$id=<?=$id ;?>" method="post">
+        Pealkiri:<input type="text" name="title" value="<?= $book["title"] ;?>">
+        <br><br>
         <button type="submit">Salvesta</button>
     </form>
 </body>
