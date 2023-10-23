801924f5f365737319da994d53b890115a79acfc
karlruutel <karl.ruutel@ametikool.ee>
2023-10-23 09:25:05 +0300

    7

--- edit.php
+++ edit.php
@@ -1,25 +1,20 @@
 <?php
 
-require_once("connect.php");
-
 $id = $_GET["id"];
-
-$stmt = $pdo->prepare('SELECT * FROM books WHERE id = :id');
-$stmt->execute(["id" => $id]);
-$book = $stmt->fetch();
 
 ?>
 
-<!DOCTYPE html>
+~<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Muuda</title>
 </head>
 <body>
-
-<h1><?= $book["title"] ?></h1>
-    <h1>Muuda</h1> <?= $id;?>
+    <form action="edit,php" method="post">
+        <input type="text" name="title" value="">
+        <button type="submit">Salvesta</button>
+    </form>
 </body>
 </html>
